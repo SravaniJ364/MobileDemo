729553f75981a6f9a809e8033342f27cc45d458e
SravaniJ364 <sravani.j@centumelectronics.com>
2026-07-27 14:47:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage(){
+
+    document.getElementById("result").innerHTML =
+        "Hello from JavaScript!";
+}
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "demo-cache-v2";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
